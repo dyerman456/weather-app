d195ea0d690379fbe0c2d6cc862cfb8cc488074a
dyerman456 <dyerman456@gmail.com>
2025-09-29 17:57:00 +0200

    chore: update dependencies and remove unused asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {City} from "./components/City";
+import {v1} from "uuid";
+
+type CityType = {
+  id: string
+  name: string,
+  N: number,
+  E: number,
+}
 
 function App() {
+
+  const cities: CityType[] = [
+    {id: v1(), name: 'Warsaw', N: 52.2297, E: 21.0122},
+    {id: v1(), name: 'Gdansk', N: 54.3520, E: 18.6466},
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="App">
+        {cities.map(city => {
+          return (
+            <City key={city.id} name={city.name} coordinates={[city.N, city.E]}/>
+          )
+        })}
+      </div>
     </div>
   );
 }
